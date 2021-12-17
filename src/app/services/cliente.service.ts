faddb3f3ca710aa4b01faf3ac7f50996aaf4c721
AntonioBuono <antoniobuono277@gmail.com>
2021-12-17 11:12:45 +0100

    Modifica 17/12

--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+thiimport { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Clienti } from '../classes/clienti';
